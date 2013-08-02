12d39827c943d091d0f6b0bdb15399b5b2e0de9e
Patrick Byrne <code@patrickbyrne.net>
2013-08-02 15:31:44 -0500

    Move logic of parsing input into own method

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -10,38 +10,34 @@ class Bob
       self.input = input
     end
 
+    RESPONSES = {
+      silence: "Fine. Be that way!",
+      anger: "Woah, chill out!",
+      question: "Sure.",
+      else: "Whatever.",
+    }
+
     def input
       @input ||= ""
     end
 
     def response
-      if input == ""
-        response_to_silence
-      elsif input == input.upcase
-        response_to_anger
-      elsif input.end_with? "?"
-        response_to_question
-      else
-        response_to_anything_else
-      end
+      RESPONSES.fetch(input_type)
     end
 
   private
 
-    def response_to_silence
-      "Fine. Be that way!"
+    def input_type
+      if input == ""
+        :silence
+      elsif input == input.upcase
+        :anger
+      elsif input.end_with? "?"
+        :question
+      else
+        :else
+      end
     end
 
-    def response_to_anger
-      "Woah, chill out!"
-    end
-
-    def response_to_question
-      "Sure."
-    end
-
-    def response_to_anything_else
-      "Whatever."
-    end
   end
 end
